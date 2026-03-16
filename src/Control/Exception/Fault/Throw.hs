656b1a8cb0b04e0a928fe55bc76c7b7ae3e51521
cmk <6869885+cmk@users.noreply.github.com>
2026-03-16 01:27:31 -0700

    Lift Throw.hs to MonadIO/MonadUnliftIO throughout
    - throwIO, throwIODefect, throwIOWithCallStack, throwIOLeft:
      now MonadIO m, not bare IO
    - catchIgnoringStack, handleIgnoringStack: now MonadUnliftIO m
    - catchIgnoringDisplay, handleIgnoringDisplay: now MonadUnliftIO m
    - displayExceptions: now MonadUnliftIO m
    - All catch/handle functions use Catch module (sync-safe) instead
      of bare Control.Exception
    - Clean separation: pure throw (with NotInIO) vs monadic throw

--- src/Control/Exception/Fault/Throw.hs
+++ src/Control/Exception/Fault/Throw.hs
@@ -3,25 +3,29 @@
            , TypeOperators
            , UndecidableInstances #-}
 
--- | Various tools for improving the quality of the failures in your programs.
---   This is meant to encourage you to "do the right thing" more often.
+-- | Type-safe throwing, call stack attachment, and display fixes.
+--
+-- This module provides:
+--
+-- * A 'throw' that gives a compile error if used in 'IO' ('NotInIO')
+-- * 'MonadIO'-lifted variants of all throwing functions
+-- * 'MonadUnliftIO'-lifted catch\/handle that see through wrappers
 module Control.Exception.Fault.Throw
-  ( -- * A better `Except.throw`
+  ( -- * Pure throw (with NotInIO guard)
     throw,
+    throwDefect,
+    throwWithCallStack,
+    throwLeft,
+    -- * Monadic throw
+    throwIO,
+    throwIODefect,
+    throwIOWithCallStack,
+    throwIOLeft,
     -- * Call stacks
     CallStackException (..),
+    addCallStack,
     catchIgnoringStack,
     handleIgnoringStack,
-    addCallStack,
-    throwWithCallStack,
-    throwIOWithCallStack,
-    -- * Lifting values to exceptions
-    Defect (..),
-    throwDefect,
-    throwIODefect,
-    -- * Disjunctions to exceptions
-    throwLeft,
-    throwIOLeft,
     -- * Display fix
     Display (..),
     displayExceptions,
@@ -34,7 +38,7 @@ where
 
 import           Control.Exception.Fault.Class
 import           Control.Exception.Fault.Wrap (CallStackException(..), Defect(..), Display(..))
-import           Control.Exception (catch, handle, throwIO)
+import qualified Control.Exception.Fault.Catch as Catch
 import qualified Control.Exception as Except
 import           Control.Monad.Trans.Accum
 import           Control.Monad.Trans.Cont
@@ -52,42 +56,99 @@ import qualified Control.Monad.Trans.Writer.CPS as CPS
 import qualified Control.Monad.Trans.Writer.Lazy as Lazy
 import qualified Control.Monad.Trans.Writer.Strict as Strict
 import Data.Kind (Constraint)
-import GHC.Stack (prettyCallStack)
 import GHC.TypeLits (ErrorMessage(..), TypeError)
-import System.Exit (exitFailure)
-import System.IO (IO, hPutStr, stderr)
 
+---------------------------------------------------------------------
+-- Pure throw (with NotInIO guard)
+---------------------------------------------------------------------
 
--- | A replacement for `Except.throw` that complains if you should be calling
---  `throwIO` instead.
+-- | A replacement for @Control.Exception.'Except.throw'@ that gives a
+-- compile error if used in 'IO'. Use 'throwIO' in monadic code instead.
 throw :: (Exception e, NotInIO a) => e -> a
 throw = Except.throw
 
--- | See `handleIgnoringStack`.
-catchIgnoringStack :: Exception e => IO a -> (e -> IO a) -> IO a
-catchIgnoringStack = flip handleIgnoringStack
+-- | Throw any 'Typeable' value as a 'Defect'. The function provides
+-- 'displayException' if uncaught.
+throwDefect :: (Typeable e, NotInIO a) => (e -> String) -> e -> a
+throwDefect f = throw . Defect f
 
--- | Like `handle`, but checks for both the "bare" and 'CallStackException'
---   versions of an exception.
---
---  __NB__: This does mean that if you re-`throwIO` you'll discard the original
---         `CallStack`.
-handleIgnoringStack :: Exception e => (e -> IO a) -> IO a -> IO a
-handleIgnoringStack f = handle f . handle (\(CallStackException e _) -> f e)
+-- | Throw an exception with the current 'CallStack' attached.
+throwWithCallStack :: (HasCallStack, Exception e, NotInIO a) => (CallStack -> e) -> a
+throwWithCallStack = throw . ($ callStack)
 
--- | Adds the current call stack to the exception.
+-- | Eliminate an 'Either' by throwing the 'Left'.
+throwLeft :: (Exception e, NotInIO a) => Either e a -> a
+throwLeft = either throw id
+
+---------------------------------------------------------------------
+-- Monadic throw
+---------------------------------------------------------------------
+
+-- | Throw an exception in any 'MonadIO'.
+throwIO :: (MonadIO m, Exception e) => e -> m a
+throwIO = Catch.throwIO
+
+-- | Throw any 'Typeable' value as a 'Defect' in 'MonadIO'.
+throwIODefect :: (MonadIO m, Typeable e) => (e -> String) -> e -> m a
+throwIODefect f = throwIO . Defect f
+
+-- | Throw an exception with the current 'CallStack' in 'MonadIO'.
+throwIOWithCallStack :: (HasCallStack, MonadIO m, Exception e) => (CallStack -> e) -> m a
+throwIOWithCallStack = throwIO . ($ callStack)
+
+-- | Eliminate an 'Either' by throwing the 'Left' in 'MonadIO'.
+throwIOLeft :: (MonadIO m, Exception e) => Either e a -> m a
+throwIOLeft = either throwIO pure
+
+---------------------------------------------------------------------
+-- Call stacks
+---------------------------------------------------------------------
+
+-- | Attach the current 'CallStack' to an exception.
 addCallStack :: HasCallStack => e -> CallStackException e
 addCallStack e = CallStackException e callStack
 
--- | Tries to prevent using non-`IO`-safe `throw` operations in `IO`. It covers
---   the standard transformers, but still can't /guarantee/ that there's no `IO`
---   in the stack, in which case the constraint name at least gives an extra
---   hint to callers. The constraint may need to be propagated and it also tries
---   to give a helpful error message when it fails.
+-- | Catch an exception, seeing through 'CallStackException' wrappers.
+--
+-- __NB__: If you re-throw, the original 'CallStack' is discarded.
+catchIgnoringStack :: (MonadUnliftIO m, Exception e) => m a -> (e -> m a) -> m a
+catchIgnoringStack = flip handleIgnoringStack
+
+-- | Handle an exception, seeing through 'CallStackException' wrappers.
+handleIgnoringStack :: (MonadUnliftIO m, Exception e) => (e -> m a) -> m a -> m a
+handleIgnoringStack f = Catch.handle f . Catch.handle (\(CallStackException e _) -> f e)
+
+---------------------------------------------------------------------
+-- Display fix
+---------------------------------------------------------------------
+
+-- | Fix @main@ to use 'displayException' instead of 'show' for uncaught exceptions.
+--
+-- @
+-- main = displayExceptions actualMain
+-- @
+--
+-- See [Why doesn't GHC use my displayException method?](https://stackoverflow.com/questions/55490766)
+displayExceptions :: MonadUnliftIO m => m a -> m a
+displayExceptions =
+  handleIgnoringDisplay (\e -> throwIO (Display (e :: SomeException)))
+
+-- | Handle an exception, seeing through 'Display' wrappers.
+handleIgnoringDisplay :: (MonadUnliftIO m, Exception e) => (e -> m a) -> m a -> m a
+handleIgnoringDisplay f = Catch.handle f . Catch.handle (\(Display e) -> f e)
+
+-- | Catch variant of 'handleIgnoringDisplay'.
+catchIgnoringDisplay :: (MonadUnliftIO m, Exception e) => m a -> (e -> m a) -> m a
+catchIgnoringDisplay = flip handleIgnoringDisplay
+
+---------------------------------------------------------------------
+-- NotInIO
+---------------------------------------------------------------------
+
+-- | Constraint that gives a compile error if the return type is @IO a@
+-- or any transformer stack over @IO@. This prevents accidental use of
+-- imprecise 'throw' where 'throwIO' is needed.
 type family NotInIO a :: Constraint where
-  -- The bulk of the cases are the long way of saying
-  -- @`MonadIO` m => m a = `TypeError` ...@ but without being able to catch any
-  -- instances we don't know about a priori.
   NotInIO (IO a) =
     TypeError
     ('Text "Can't use a `throw` function in `" ':<>:
@@ -109,69 +170,4 @@ type family NotInIO a :: Constraint where
   NotInIO (Strict.RWST _ w s m a) = NotInIO (m (a, s, w))
   NotInIO (Strict.StateT s m a) = NotInIO (m (a, s))
   NotInIO (Strict.WriterT w m a) = NotInIO (m (a, w))
-  -- and otherwise we're safe ... relatively
   NotInIO _ = ()
-
--- | Throws any type. The provided function is what's used for serialization if
---   the exception isn't caught.
-throwDefect :: (Typeable e, NotInIO a) => (e -> String) -> e -> a
-throwDefect f = throw . Defect f
-
--- | See 'throwDefect'.
-throwIODefect :: Typeable e => (e -> String) -> e -> IO a
-throwIODefect f = throwIO . Defect f
-
--- | Throws an exception that's expecting to carry a `CallStack`.
---
---   I /think/ these two should be equivalent
---
--- > throwWithCallStack . CallStackException
--- > throw . addCallStack
-throwWithCallStack :: (HasCallStack, Exception e, NotInIO a) => (CallStack -> e) -> a
-throwWithCallStack = throw . ($ callStack)
-
--- | See `throwWithCallStack`.
-throwIOWithCallStack :: (HasCallStack, Exception e) => (CallStack -> e) -> IO a
-throwIOWithCallStack = throwIO . ($ callStack)
-
--- | Eliminate an `Either` by throwing the left.
-throwLeft :: (Exception e, NotInIO a) => Either e a -> a
-throwLeft = either throw id
-
--- | See `throwLeft`.
-throwIOLeft :: Exception e => Either e a -> IO a
-throwIOLeft = either throwIO pure
-
--- | This "fixes" @main@ to use `displayException` instead of `show` when
---   failing with an exception.
---
---   E.g., replace @main = x@ with @main = displayExceptions $ x@. This is also
---   useful before `unsafePerformIO` if you won't get control back again (like,
---   in a GHC plugin).
---
---   This works by wrapping the `Exception` in a newtype that has the correct
---   behavior, which means that if you want to try handling these exceptions
---   outside a call to `displayExceptions`, you should be using
---  `handleIgnoringDisplay` (or `catchIgnoringDisplay`).
---
---   See [Why doesn’t GHC use my `displayException`
---   method?](https://stackoverflow.com/questions/55490766/why-doesn-t-ghc-use-my-displayexception-method)
---   for some explanation. I find it totally unconvincing, and I think Kmett's
---   comment about "\'helpful\' `Show` instances" makes the argument /for/ using
---  `displayException` -- with the current behavior, users are encouraged to
---   define a custom `show` to get GHC to output a useful failure message, which
---   then breaks the /intended/ use of `show` as an syntax printer.
-displayExceptions :: IO a -> IO a
-displayExceptions =
-  -- Using `handleIgnoringDisplay` in order to avoid re-wrapping already-wrapped
-  -- exceptions.
-  handleIgnoringDisplay (\e -> throwIO (Display (e :: SomeException)))
-
--- | Handles an exception that /may/ be wrapped in the `Display` @newytpe@,
---   unwrapping it if necessary.
-handleIgnoringDisplay :: Exception e => (e -> IO a) -> IO a -> IO a
-handleIgnoringDisplay f = handle f . handle (\(Display e) -> f e)
-
--- | See `handleIgnoringDisplay`.
-catchIgnoringDisplay :: Exception e => IO a -> (e -> IO a) -> IO a
-catchIgnoringDisplay = flip handleIgnoringDisplay
